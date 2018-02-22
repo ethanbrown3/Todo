026be8a473f0c4a55420ba55591ad19f28eba255
Ethan Brown <ethanbrown3@mail.weber.edu>
2018-02-21 23:33:43 -0700

    Wired up Table View and populated with dummy array

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -9,17 +9,26 @@
 import UIKit
 
 class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["Just Send it", "Open Door", "Plant Defuser"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
     }
-
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
 
 }
 
