d7e9bb51e273530e96a44caec7d3d96c95625603
Ethan Brown <ethanbrown3@mail.weber.edu>
2018-05-02 18:10:56 -0600

    Added checkmark feature to todo list items

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -29,6 +29,16 @@ class TodoListViewController: UITableViewController {
         
         return cell
     }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print(itemArray[indexPath.row])
+        if (tableView.cellForRow(at: indexPath)?.accessoryType != .checkmark) {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 
 }
 
